041e14964a873b24337c0e6d09b5891bfeaad1ea
«Maxim» <kuzmax07@mail.ru>
2022-06-01 14:53:10 +0300

    add Example 005 Condition If - Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "мария")
+{
+    Console.WriteLine("Ура, это же Маша ");
+}
+else
+{
+    Console.WriteLine("Привет, как дела? ");
+}
